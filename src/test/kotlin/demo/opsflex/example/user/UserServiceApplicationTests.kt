5c30139d20dd55c15750dd0b800328c7d10c2e65
seonbo.shim <seonbo.shim@bespinglobal.com>
2020-11-25 03:50:12 +0900

    init project

---
+++ src/test/kotlin/demo/opsflex/example/user/UserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package demo.opsflex.example.user
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
